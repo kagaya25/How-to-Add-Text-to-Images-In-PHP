d992055d7bfc3da58819b0128a5fc7f658493cd7
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-09-28 13:35:19 +0900

    update
    update

--- main.php
+++ main.php
@@ -1,15 +1,19 @@
 <?php
-//  OPEN IMAGE
+// (A) OPEN IMAGE
 $img = imagecreatefromjpeg('sample.jpg');
-//  WRITE TEXT
+
+// (B) WRITE TEXT
 $white = imagecolorallocate($img, 255, 255, 255);
 $txt = "1,000,000 Views";
 $font = "C:\Windows\Fonts\arial.ttf"; 
+
 //  THE TEXT Position
 $centerX = 204;
 $centerY = 228;
 imagettftext($img, 80, 0, $centerX, $centerY, $white, $font, $txt);
-// OUTPUT IMAGE
+
+// (C) OUTPUT IMAGE
+
 imagejpeg($img);
 imagejpeg($img, "test.jpg", 100);
 imagedestroy($img);
