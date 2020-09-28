a55c2fafddeea4e37893733b7452dcd17b094cb1
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-09-28 13:56:23 +0900

    add next line text
    add next line text

--- main.php
+++ main.php
@@ -1,19 +1,16 @@
 <?php
-// (A) OPEN IMAGE
+//OPEN IMAGE
 $img = imagecreatefromjpeg('sample.jpg');
-
-// (B) WRITE TEXT
+//WRITE TEXT
 $white = imagecolorallocate($img, 255, 255, 255);
 $txt = "1,000,000 Views";
 $font = "C:\Windows\Fonts\arial.ttf"; 
-
-//  THE TEXT Position
-$centerX = 204;
-$centerY = 228;
-imagettftext($img, 80, 0, $centerX, $centerY, $white, $font, $txt);
-
-// (C) OUTPUT IMAGE
-
+//THE TEXT Position
+$positionX = 204;
+$positionY = 228;
+imagettftext($img, 80, 0, $positionX, $positionY, $white, $font, $txt);
+imagettftext($img, 80, 0, 204, 328, $white, $font, $txt);
+//OUTPUT IMAGE
 imagejpeg($img);
 imagejpeg($img, "test.jpg", 100);
 imagedestroy($img);
