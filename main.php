38c5f1718d2a068c1b369d7a0b7fbf2556eb62c6
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-09-28 13:34:04 +0900

    Delete some comments we dont needs
    Delete some comments we dont needs

--- main.php
+++ main.php
@@ -1,19 +1,15 @@
 <?php
-// (A) OPEN IMAGE
+//  OPEN IMAGE
 $img = imagecreatefromjpeg('sample.jpg');
-
-// (B) WRITE TEXT
+//  WRITE TEXT
 $white = imagecolorallocate($img, 255, 255, 255);
 $txt = "1,000,000 Views";
 $font = "C:\Windows\Fonts\arial.ttf"; 
-
 //  THE TEXT Position
 $centerX = 204;
 $centerY = 228;
 imagettftext($img, 80, 0, $centerX, $centerY, $white, $font, $txt);
-
-// (C) OUTPUT IMAGE
-
+// OUTPUT IMAGE
 imagejpeg($img);
 imagejpeg($img, "test.jpg", 100);
 imagedestroy($img);
